790ae048d29029b289b7b89f371fd8215e48e841
Antoine Carrette <71089756+Eriuden@users.noreply.github.com>
2023-04-25 20:34:18 +0200

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 mongoose
 .connect(
-    "mongodb+srv://" + process.env.DB_USER_PASS + "@cluster0.iodcc.mongodb.net/Onwine",
+    ,
 )
 .then(()=> console.log("connecté à MongoDB"))
 .catch((err) => console.log("échec de connexion", err))
