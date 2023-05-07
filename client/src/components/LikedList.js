7a3bec79ece806ec1b7770b67a642698c5c348d2
Eriuden <antoinecarrette@hotmail.fr>
2023-05-07 17:17:39 +0200

    home

---
+++ client/src/components/LikedList.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LikedList = () => {
+  return (
+    <div>LikedList</div>
+  )
+}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,46 @@
 import React from 'react'
+import {useDispatch, useSelector} from "react-redux";
+import { WineCard } from '../components/WineCard';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { getWine } from '../../../server/controllers/wine.controller';
+import { isEmpty } from '../utils';
 
 export const Home = () => {
+  const [loadWine, setLoadWine] = useState(false)
+  const [count, setCount] = useState(0)
+  const dispatch = useDispatch()
+  const wines = useSelector((state)=> state.allWinesReducer)
+
+  const loadMore = () => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop + 1 >
+      document.scrollingElement.scrollHeight
+    ) {
+      setLoadWine(true)
+    }
+  }
+
+  useEffect(()=> {
+    if (loadWine) {
+      dispatch(getWine(count))
+      setLoadWine(false)
+      setCount(count+12)
+    }
+
+    window.addEventListener("scroll", loadMore)
+  }, [loadWine, dispatch, count])
+
   return (
-    <div>Home</div>
+    <div>
+      <div>
+        <ul>
+          {!isEmpty(wines[0]) &&
+          wines.map((wine)=> {
+            return <WineCard props={wine} key={wine._id}/>
+          })}
+        </ul>
+      </div>
+    </div>
   )
 }
