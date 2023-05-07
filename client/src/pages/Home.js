ff82231a22376030859ab2f54f04982b90e13dec
Eriuden <antoinecarrette@hotmail.fr>
2023-05-07 17:34:12 +0200

    some explanations

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,6 +13,17 @@ export const Home = () => {
   const wines = useSelector((state)=> state.allWinesReducer)
 
   const loadMore = () => {
+    /* window.innerHeight = hauteur intérieure de la fenètre en px
+     document.documentElement.scrollTop = en px aussi,
+     à combien de px on est du haut de l'élément parent principal de la page
+     sachant qu'on est plus en haut, et bien, si on scroll
+     scrollingElement c'est l'élement qui fait défiler le doc
+     C'est en quelque sorte documentElement mais dans le contexte d'un scroll
+     scrollHeight, la hauteur dont il a besoin pour tout afficher
+     de cette manière, il évitera de charger les cartes 
+     AVANT que l'on ait scrollé jusqu'au bout
+     puisque il faut dépasse la hauteur minimale calculée de l'élément 
+    */
     if (
       window.innerHeight + document.documentElement.scrollTop + 1 >
       document.scrollingElement.scrollHeight
